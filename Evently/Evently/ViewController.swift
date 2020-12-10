1585b55ad7feae805eb41558c6623e594a3eb8d4
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-12-09 23:41:29 -0500

    Initial Commit
    Added the scaffold for the xcode project

---
+++ Evently/Evently/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Evently
+//
+//  Created by Ricardo Carrillo on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
